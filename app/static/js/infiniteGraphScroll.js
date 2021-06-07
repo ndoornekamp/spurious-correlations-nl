b2f6e6435dc6d335e7b64c162f4b39e96adb086d
Nick Doornekamp <nddoornekamp@gmail.com>
2021-06-07 11:13:53 -0700

    Add download button to graphs

--- app/static/js/infiniteGraphScroll.js
+++ app/static/js/infiniteGraphScroll.js
@@ -1,103 +1,144 @@
 const graphContainer = document.getElementById("graph-container");
-let nofPlots = 5
+let nofPlots = 5;
 
 function addCanvas() {
-    const div = document.createElement('div')
-    const canvas = document.createElement("canvas")
-    canvas.classList.add('line-chart')
-    div.append(canvas)
-    graphContainer.appendChild(div)
-    return canvas
+	const div = document.createElement("div");
+	div.classList.add("graph");
+	const canvas = document.createElement("canvas");
+	canvas.classList.add("line-chart");
+	div.append(canvas);
+	graphContainer.appendChild(div);
+
+	return canvas;
 }
 
 async function addPlot(datasetIndex) {
-    canvas = addCanvas()
-    const ctx = canvas.getContext("2d")
+	canvas = addCanvas();
+	const ctx = canvas.getContext("2d");
 
-    dataset = await fetchData(datasetIndex)
-    const data = {
-        datasets: [
-        {
-          data: dataset.series1.values,
-          label: dataset.series1.localized_axis_label,
-          yAxisID: 'y',
-          backgroundColor: '#CD5C5C',
-          borderColor: '#CD5C5C',
-        },
-        {
-          data: dataset.series2.values,
-          label: dataset.series2.localized_axis_label,
-          yAxisID: 'y1',
-          backgroundColor: '#ADD8E6',
-          borderColor: '#ADD8E6',
-        }],
-        labels: [2014, 2015, 2016, 2017, 2018, 2019, 2020]
-    };
-    const title = [dataset.series1.localized_title, " vs. ", dataset.series2.localized_title, 'ρ = ' + dataset.correlation.toFixed(4)]
+	dataset = await fetchData(datasetIndex);
+	const data = {
+		datasets: [
+			{
+				data: dataset.series1.values,
+				label: dataset.series1.localized_axis_label,
+				yAxisID: "y",
+				backgroundColor: "#CD5C5C",
+				borderColor: "#CD5C5C",
+			},
+			{
+				data: dataset.series2.values,
+				label: dataset.series2.localized_axis_label,
+				yAxisID: "y1",
+				backgroundColor: "#ADD8E6",
+				borderColor: "#ADD8E6",
+			},
+		],
+		labels: [2014, 2015, 2016, 2017, 2018, 2019, 2020],
+	};
 
-    const config = {
-    type: 'line',
-    data: data,
-    options: {
-        responsive: true,
-        interaction: {
-            mode: 'index',
-            intersect: false,
-        },
-        stacked: false,
-        plugins: {
-            title: {
-                display: true,
-                text: title
-            }
-        },
-        scales: {
-              y: {
-                  type: 'linear',
-                  display: true,
-                  position: 'left',
-                  title: {
-                    display: true,
-                    text: dataset.series1.localized_axis_label
-                  },
-              },
-              y1: {
-                  type: 'linear',
-                  display: true,
-                  position: 'right',
-                  title: {
-                    display: true,
-                    text: dataset.series2.localized_axis_label
-                  },
+	const title = [
+		dataset.series1.localized_title,
+		" vs. ",
+		dataset.series2.localized_title,
+		"ρ = " + dataset.correlation.toFixed(4),
+	];
 
-                  // grid line settings
-                  grid: {
-                      drawOnChartArea: false, // only want the grid lines for one axis to show up
-                    },
-              },
-            }
-        },
-    };
-    new Chart(ctx, config)
+	const canvasBackgroundColorPlugin = {
+		id: "custom_canvas_background_color",
+		beforeDraw: (chart) => {
+			const ctx = chart.canvas.getContext("2d");
+			ctx.save();
+			ctx.globalCompositeOperation = "destination-over";
+			ctx.fillStyle = "white";
+			ctx.fillRect(0, 0, chart.width, chart.height);
+			ctx.restore();
+		},
+	};
+
+	const config = {
+		type: "line",
+		data: data,
+		plugins: [canvasBackgroundColorPlugin],
+		options: {
+			responsive: true,
+			interaction: {
+				mode: "index",
+				intersect: false,
+			},
+			stacked: false,
+			plugins: {
+				title: {
+					display: true,
+					text: title,
+				},
+			},
+			scales: {
+				y: {
+					type: "linear",
+					display: true,
+					position: "left",
+					title: {
+						display: true,
+						text: dataset.series1.localized_axis_label,
+					},
+				},
+				y1: {
+					type: "linear",
+					display: true,
+					position: "right",
+					title: {
+						display: true,
+						text: dataset.series2.localized_axis_label,
+					},
+
+					// grid line settings
+					grid: {
+						drawOnChartArea: false, // only want the grid lines for one axis to show up
+					},
+				},
+			},
+		},
+	};
+
+	const chart = new Chart(ctx, config);
+
+	return [canvas, chart];
 }
 
 async function fetchData(datasetIndex) {
-    const response = await fetch('/data', {method: 'POST', body: JSON.stringify({index: datasetIndex})})
-    const data = await response.json()
-    return data
+	const response = await fetch("/data", {
+		method: "POST",
+		body: JSON.stringify({ index: datasetIndex }),
+	});
+	const data = await response.json();
+	return data;
 }
 
-window.onscroll = function(ev) {
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        nofPlots++
-        addPlot(nofPlots)
-    }
+function addDownloadButton(canvas, chart) {
+	const downloadButton = document.createElement("button");
+	downloadButton.onclick = function () {
+		const a = document.createElement("a");
+		a.href = chart.toBase64Image();
+		a.download = "correlation.png";
+		a.click();
+	};
+	canvas.parentElement.append(downloadButton);
+}
+
+window.onscroll = async function (ev) {
+	if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+		nofPlots++;
+		const [canvas, chart] = await addPlot(nofPlots);
+		addDownloadButton(canvas, chart);
+	}
 };
 
-function init(initialNofPlots) {
-    for (i = 0; i < initialNofPlots; i++) {
-        addPlot(i)
-    }
+async function init(initialNofPlots) {
+	for (i = 0; i < initialNofPlots; i++) {
+		const [canvas, chart] = await addPlot(i);
+		addDownloadButton(canvas, chart);
+	}
 }
 
 init(nofPlots);
